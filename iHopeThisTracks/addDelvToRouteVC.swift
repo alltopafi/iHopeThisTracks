d6dc4f99974c9aa94085aa8d4d6b34dd48e3c645
Jesse Alltop <Jesse@macbook-pro-2.local>
2016-04-21 15:49:34 -0500

    create route view added,but we need to only show active deliveries that are not assigned to someone, need to implement when clicking on a tab it will send a query to  the database to create a delivery from that invoice with the drivers name(I think we could remove set name button) then my route tab needs to altered to show only open deliveries from name in the box(again i think we could remove the set driver button) finally, we need to add the pins to the map(with database alterations)

---
+++ iHopeThisTracks/addDelvToRouteVC.swift
@@ -0,0 +1,94 @@
+//
+//  addDelvToRouteVC.swift
+//  iHopeThisTracks
+//
+//  Created by Jesse Alltop on 4/21/16.
+//  Copyright © 2016 it391. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class addDelvToRouteVC: UIViewController, UITableViewDataSource,UITextFieldDelegate, UITableViewDelegate, deliveryModelProtocal
+{
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBOutlet weak var listTableView: UITableView!
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        nameField.resignFirstResponder()
+        return true
+    }
+    @IBAction func setNameButton(sender: AnyObject) {
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //set delegates and initialize homeModel
+        
+        self.listTableView.delegate = self
+        self.listTableView.dataSource = self
+        
+        myDeliveryModel.delegate = self
+        myDeliveryModel.downloadItems()
+        sleep(1)
+        feedItems = myDeliveryModel.getDeliveries()
+        //        print(feedItems.count)
+        
+    }
+    
+    func itemsDownloaded(items: NSArray) {
+        feedItems = items
+        self.listTableView.reloadData()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of feed items
+        return feedItems.count
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // Retrieve cell
+        let cellIdentifier: String = "BasicCellRoute"
+        let myCell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier)!
+        
+        // Get the location to be shown
+        let item: deliveryHelper = feedItems[indexPath.row] as! deliveryHelper
+        // Get references to labels of cell
+        myCell.textLabel!.text = item.DESTINATION
+        return myCell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let currentCell = tableView.cellForRowAtIndexPath(indexPath)! as UITableViewCell
+        //        print("selected: ",currentCell.textLabel!.text)
+        
+        for(var i = 0; i<feedItems.count; i++)
+        {
+            if(feedItems[i].DESTINATION == currentCell.textLabel!.text)
+            {
+                print("index is at: ",i)
+                index = i
+                break;
+            }
+        }
+        
+        switchScreen();
+        
+    }
+    
+    
+    func switchScreen() {
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: NSBundle.mainBundle())
+        let vc : UIViewController = mainStoryboard.instantiateViewControllerWithIdentifier("myDeliveryViewController") as UIViewController
+        
+        
+        self.presentViewController(vc, animated: true, completion: nil)
+    }
+
+    
+}
